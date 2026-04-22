c840bcc42f5cc162fcdea191f6a5a95854aa47a1
Naman Joshi <namanjosji9@gmail.com>
2026-04-22 23:58:40 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import React, { useState } from 'react'
-import Heading from './Components/Heading'
-import AddStudent from './Components/AddStudent'
-import StudentsSummary from './Components/StudentSummary'
-import AllDetails from './Components/AllDetails'
-
-function App() {
-
+import Heading from './components/Heading';
+import AddStudent from './components/AddStudent';
+import StudentSummary from './components/StudentSummary';
+import AllDetais from './components/AllDetails';
+function App(){
   const initialStudents = [
-    {id:1,name:"Naman",score:78},
-    {id:2,name:"Pawan",score:98},
-    {id:3,name:"Dhruv",score:9}
+    {id:1,name:"Aman",score:78},
+    {id:2,name:"Alex",score:38},
+    {id:3,name:"Kai",score:55}
   ]
-
-  const [students,setStudents] = useState(initialStudents)
-
+  const[students,setStudents] = useState(initialStudents)
   const updateScore = (index,newScore)=>{
-    const updated = students.map(s=>s.id === index+1 ? {...s,score:Number(newScore)}:s)
-
+    const updated = students.map((s,i) => i === index ? {...s,score:Number(newScore)}:s)
     setStudents(updated)
   }
 
-  return (
-    <div className='w-full flex flex-col items-center gap-10 p-20 h-full bg-gray-900'>
-    <Heading/>
-    <AddStudent students={students} setStudents={setStudents}/>
-    <StudentsSummary students={students}/>
-    <AllDetails students={students} updateScore={updateScore}/>
+  return(
+    <div className="min-h-screen w-full flex flex-col items-center gap-10 py-10 px-4 bg-gradient-to-br from-gray-900 via-[#0a192f] to-black">
+      <Heading/>
+      <AddStudent students={students} setStudents={setStudents}/>
+      <StudentSummary students={students}/>
+      <AllDetais students={students} updateScore={updateScore}/>
     </div>
+
   )
 }
-
-export default App
+export default App;
