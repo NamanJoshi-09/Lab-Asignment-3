0abef67f5a1c408fcc59f3ce0c294e98d0ca759a
Naman Joshi <namanjosji9@gmail.com>
2026-04-22 23:49:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
 import Heading from './Components/Heading'
 import AddStudent from './Components/AddStudent'
-import StudentsSummary from './Components/StudentsSummary'
+import StudentsSummary from './Components/StudentSummary'
 import AllDetails from './Components/AllDetails'
 
 function App() {
 
   const initialStudents = [
     {id:1,name:"Naman",score:78},
-    {id:2,name:"Addi",score:58},
-    {id:3,name:"Dhruv",score:39}
+    {id:2,name:"Pawan",score:98},
+    {id:3,name:"Dhruv",score:9}
   ]
 
   const [students,setStudents] = useState(initialStudents)
 
-  const updateScore = (index, newScore) => {
-  const updated = students.map((s, i) =>
-    i === index ? { ...s, score: Number(newScore) } : s
-  )
-  setStudents(updated)
-}
+  const updateScore = (index,newScore)=>{
+    const updated = students.map(s=>s.id === index+1 ? {...s,score:Number(newScore)}:s)
+
+    setStudents(updated)
+  }
 
 
  
@@ -29,7 +28,7 @@ function App() {
     <div className='w-full flex flex-col items-center gap-10 p-20 h-full bg-gray-900'>
     <Heading/>
     <AddStudent students={students} setStudents={setStudents}/>
-    <StudentsSummary/>
+    <StudentsSummary students={students}/>
     <AllDetails students={students} updateScore={updateScore}/>
     </div>
   )
